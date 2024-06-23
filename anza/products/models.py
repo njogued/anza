432ecd58e4f05dbc109df079ca86a79e23c1f660
njogued <njogued@gmail.com>
2024-06-23 15:24:44 +0300

    new model: ProductImage model to save multiple product images

--- anza/products/models.py
+++ anza/products/models.py
@@ -3,11 +3,10 @@ from business.models import Business, Category
 from users.models import CustomUser
 
 class Product(models.Model):
-    product_id = models.AutoField(primary_key=True, default=0)
+    product_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='product_images/')
     quantity = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     business = models.ForeignKey(Business, on_delete=models.CASCADE, related_name='products')
@@ -17,6 +16,15 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='product_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product.name + ' - ' + self.image.name
 
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
--- anza/products/views.py
+++ anza/products/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from .models import Product, Review, ProductImage
+from .forms import CreateProductForm, ProductImageForm
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
+class CreateProductView(LoginRequiredMixin, CreateView):
+    def get(self, request):
+        # Provide the user with a form to create a new product
+        product_form = self.form_class()
+        image_form = ProductImageForm()
+        return render(request, self.template_name, {"product_form": product_form, "image_form": image_form})
+    
+    def post(self, request):
+        # Handle the form submission
+        curr_user = request.user
+        product_form = self.form_class(request.POST, request.FILES)
+        if product_form.is_valid():
+            product = product_form.save()
+            images = request.FILES.getlist('image')
+            for image in images:
+                ProductImage.objects.create(product=product, image=image)
+            success_url = reverse('detail_product', kwargs={'product_id': product.product_id})
+            return redirect(success_url)
+        return render(request, self.template_name, {"form": form})
+    model = Product
+    form_class = CreateProductForm
+    success_url = reverse_lazy("home")
+    template_name = "create_product.html"
+    login_url = "/users/login"
