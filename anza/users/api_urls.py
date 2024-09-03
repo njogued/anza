f977388cb90e475e7cd5bbbfe953948d1a168c74
njogued <njogued@gmail.com>
2024-09-03 13:03:11 +0300

    get auth token route

--- anza/users/api_urls.py
+++ anza/users/api_urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import UserListView
 from .models import CustomUser
 from .serializer import UserSerializer
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('list/', UserListView.as_view(), name='user-list'),
+    path('api-token-auth/', obtain_auth_token, name='api-token-auth'),
 ]
