03a6f6bd25f7d2254484e28625f4bfe9911ffc48
njogued <njogued@gmail.com>
2024-09-08 01:51:58 +0300

    products api urls

--- anza/products/api_urls.py
+++ anza/products/api_urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import APIProductListView, APIProductDetailView
 
 urlpatterns = [
-    path("list/", APIProductListView.as_view(), name="products"),
+    path("", APIProductListView.as_view(), name="products"),
+    path("all/", APIProductListView.as_view(), name="products"),
     path("<int:pk>/", APIProductDetailView.as_view(), name="product"),
 ]
