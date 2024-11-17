450044b0034f2cb859a03ba15b046e87cdf23ae5
njogued <njogued@gmail.com>
2024-11-17 03:59:34 +0300

    user edit success

--- anza/users/forms.py
+++ anza/users/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 from django import forms
+from django.core.exceptions import ValidationError
 from django.contrib.auth import authenticate
 from .models import CustomUser
 
@@ -17,10 +18,46 @@ class CustomUserCreationForm(UserCreationForm):
     
     def clean_username(self):
         username = self.cleaned_data.get('username')
+        banned_usernames = [
+            "admin", "superuser", "staff", "user", "root", "rootuser", 
+            "adminuser", "superuser", "staffuser", "useruser", "rootadmin", "rootstaff", "profile"]
+        if username in banned_usernames:
+            raise forms.ValidationError("Username cannot be {}".format(username))
         if CustomUser.objects.filter(username=username).exists():
             raise forms.ValidationError("Username already exists")
         return username
+    
+class CustomUserUpdateForm(forms.ModelForm):
+    # A custom user form to handle user updates
+    class Meta:
+        model = CustomUser
+        fields = (
+            'first_name', 'last_name', 'phone_number', 'profile_picture',
+            'tiktok_link', 'instagram_link', 'twitter_link', 'facebook_link', 
+            'linkedin_link', 'whatsapp_link', 'website_link'
+        )
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if CustomUser.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email already exists")
+        return email
+    
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if CustomUser.objects.filter(username=username).exists():
+            raise forms.ValidationError("Username already exists")
+        return username
+    
+    def clean_profile_picture(self):
+        profile_picture = self.cleaned_data.get('profile_picture')
+        if profile_picture:
+            if profile_picture.size > 5 * 1024 * 1024:  # 5MB max size
+                raise ValidationError("Profile picture size should not exceed 5MB.")
+            # How to validate if valid file??
+            # if profile_picture.get('content_type') and not profile_picture.get('content_type').startswith("image/"):
+            #     raise ValidationError("Only image files are allowed.")
+        return profile_picture
 
 class CustomUserChangeForm(UserChangeForm):
     # A custom user form to handle user changes
--- anza/users/views.py
+++ anza/users/views.py
@@ -3,9 +3,9 @@ from django.contrib.auth import login, authenticate
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.views.generic.detail import DetailView
-from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from .forms import CustomUserCreationForm, CustomAuthenticationForm, CustomUserUpdateForm
 from .models import CustomUser
-from django.http import Http404, JsonResponse
+from django.http import Http404, JsonResponse, HttpResponse
 from django.contrib.auth import views as auth_views
 from .serializer import UserSerializer
 from rest_framework import generics
@@ -75,24 +75,56 @@ class UserDetailView(DetailView):
     context_object_name = 'user_profile'
     slug_field = 'username'
     slug_url_kwarg = 'username'
+    form_class = CustomUserUpdateForm
     def get_object(self, queryset=None):
         try:
             print(self.request)
-            return super().get_object(queryset)
+            return { "user_profile": super().get_object(queryset), "form": CustomUserUpdateForm() }
         except CustomUser.DoesNotExist:
             raise Http404("User not found")
+        
+    def post(self, request, *args, **kwargs):
+        user_profile = CustomUser.objects.get(email=self.request.user)
+        form = self.form_class(request.POST, request.FILES, instance=user_profile)
+        if form.is_valid():
+            form.save()
+            # need headers to detect ajax request
+            # if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            #     return JsonResponse({"message": "Success"}, status=200)
+            return JsonResponse({"message": "Success"}, status=200)
+        else:
+            errors = form.errors.get_json_data()
+            print(errors)
+            # need headers to detect ajax request
+            # if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            #     return JsonResponse(errors, status=400, safe=False)
+            return JsonResponse(errors, status=400, safe=False)
         
 class MyProfileView(LoginRequiredMixin, DetailView):
     model = CustomUser
     template_name = "user_profile.html"
     context_object_name = 'user_profile'
     login_url = '/users/login'
+    form_class = CustomUserUpdateForm
     def get_object(self, queryset=None):
         try:
-            user_profile = { "user_profile": CustomUser.objects.get(email=self.request.user) }
+            user_profile = { "user_profile": CustomUser.objects.get(email=self.request.user), "form": CustomUserUpdateForm() }
             return user_profile
         except CustomUser.DoesNotExist:
             raise Http404("Profile not found")
+        
+    def post(self, request, *args, **kwargs):
+        user_profile = CustomUser.objects.get(email=self.request.user)
+        form = self.form_class(request.POST, request.FILES, instance=user_profile)
+        print(request.headers.get('X-Requested-With'))
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+            # return JsonResponse({"message": "Success"}, status=200)
+        else:
+            errors = form.errors.get_json_data()
+            print(errors)
+            return JsonResponse(errors, status=400, safe=False)
     
 class CustomPasswordResetView(auth_views.PasswordResetView):
     template_name = "password_reset.html"
