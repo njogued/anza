292ed2e7063374b63091c27f6d16e8be651b32ac
njogued <njogued@gmail.com>
2024-11-25 00:10:32 +0300

    update settings.py staticfiles location

--- anza/anza/settings.py
+++ anza/anza/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'anza-html/static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
