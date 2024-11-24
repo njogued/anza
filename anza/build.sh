3a0fd8dd3058e34aae0fc617de19dc8587ef881c
njogued <njogued@gmail.com>
2024-11-25 00:05:55 +0300

    build file update

--- anza/build.sh
+++ anza/build.sh
@@ -4,8 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic
-
-python manage.py makemigrations
+python manage.py collectstatic --noinput
 
 python manage.py migrate
